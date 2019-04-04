c3b5488377ba48e53e78628098ddb0439ead5b2b
ashirko <antonshirko@frtk.ru>
2019-04-04 11:39:47 +0300

    add tests for EGTS.Parse

--- egts_test.go
+++ egts_test.go
@@ -31,7 +31,13 @@ func TestEGTS_Parse(t *testing.T) {
 		wantErr     bool
 	}{
 		{"result", packetResult(), []byte{1, 2, 3}, egtsRes(), false},
-		{"posData", packetPosData(), []byte{}, egtsPosData(), false},
+		{"posData", packetPosData(), nil, egtsPosData(), false},
+		{"signatureNotFound", egtsWithoutSignature(), nil, new(EGTS), true},
+		{"shortVeryPacket", egtsVeryShort(), egtsVeryShort(), new(EGTS), true},
+		{"shortHeader", egtsShortHeader(), nil, new(EGTS), true},
+		{"shortBody", egtsShortBody(), nil, new(EGTS), true},
+		{"incorrectHeaderCrc", egtsIncorrectHeaderCrc(), nil, new(EGTS), true},
+		{"incorrectBodyCrc", egtsIncorrectBodyCrc(), nil, new(EGTS), true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -55,8 +61,36 @@ func packetResult() []byte {
 	return []byte{1, 0, 3, 11, 0, 16, 0, 6, 0, 0, 22, 6, 0, 0, 6, 0, 6, 0, 24, 2, 2, 0, 3, 0, 6, 0, 0, 24, 29, 1, 2, 3}
 }
 
+func egtsWithoutSignature() []byte {
+	return []byte{4, 4, 4, 4, 4, 5, 6, 34, 3, 4, 5, 6, 4, 3, 3, 2, 2, 5, 6, 2, 3, 5, 6, 6, 2, 11, 4, 5, 6, 2, 41, 44}
+}
+
 func packetPosData() []byte {
 	return []byte{1, 0, 0, 11, 0, 35, 0, 0, 0, 1, 153, 24, 0, 0, 0, 1, 239, 0, 0, 0, 2, 2,
+		16, 21, 0, 210, 49, 43, 16, 79, 186, 58, 158, 210, 39, 188, 53, 3, 0, 0, 178, 0, 0, 0, 0, 0, 106, 141}
+}
+
+func egtsIncorrectHeaderCrc() []byte {
+	return []byte{1, 0, 0, 11, 0, 35, 0, 0, 0, 2, 153, 24, 0, 0, 0, 1, 239, 0, 0, 0, 2, 2,
+		16, 21, 0, 210, 49, 43, 16, 79, 186, 58, 158, 210, 39, 188, 53, 3, 0, 0, 178, 0, 0, 0, 0, 0, 106, 141}
+}
+
+func egtsIncorrectBodyCrc() []byte {
+	return []byte{1, 0, 0, 11, 0, 35, 0, 0, 0, 1, 153, 24, 0, 0, 0, 1, 239, 0, 0, 0, 2, 2,
+		16, 21, 0, 210, 49, 43, 16, 79, 186, 58, 158, 211, 39, 188, 53, 3, 0, 0, 178, 0, 0, 0, 0, 0, 106, 141}
+}
+
+func egtsShortBody() []byte {
+	return []byte{1, 0, 0, 11, 0, 36, 0, 0, 0, 1, 153, 24, 0, 0, 0, 1, 239, 0, 0, 0, 2, 2,
+		16, 21, 0, 210, 49, 43, 16, 79, 186, 58, 158, 210, 39, 188, 53, 3, 0, 0, 178, 0, 0, 0, 0, 0, 106, 141}
+}
+
+func egtsVeryShort() []byte {
+	return []byte{1, 0, 0}
+}
+
+func egtsShortHeader() []byte {
+	return []byte{1, 0, 0, 10, 0, 35, 0, 0, 0, 1, 153, 24, 0, 0, 0, 1, 239, 0, 0, 0, 2, 2,
 		16, 21, 0, 210, 49, 43, 16, 79, 186, 58, 158, 210, 39, 188, 53, 3, 0, 0, 178, 0, 0, 0, 0, 0, 106, 141}
 }
 
