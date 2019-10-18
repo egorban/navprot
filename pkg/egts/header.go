082ad40888223a15c64c290ff55f7d9dce7d68f2
ashirko <antonshirko@frtk.ru>
2019-10-18 21:07:46 +0300

    Return buff if packet is shorter then bodyLen

--- pkg/egts/header.go
+++ pkg/egts/header.go
@@ -38,6 +38,7 @@ func (packetData *Packet) parseHeader(message []byte) (body, restBuf []byte, err
 	startBody := index + headerLen
 	bodyLen := int(binary.LittleEndian.Uint16(header[5:7]))
 	if len(message[startBody:]) < bodyLen+2 {
+		restBuf = append([]byte(nil), message...)
 		err = errors.New("message is too short")
 		return
 	}
